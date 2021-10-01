fe1c75c8e46a13e0e34c3dd77432ecb99577d844
Alex Chika <mcluckey1@gmail.com>
2021-10-01 17:31:39 +0100

    first commit

---
+++ web.js
@@ -0,0 +1,5 @@
+let navToggle = document.getElementById("nav-toggle");
+let navBtn = document.getElementById("nav-btn");
+navBtn.addEventListener("click", function (e) {
+  navToggle.classList.toggle("nav-toggle");
+});
