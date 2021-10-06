668d591c8a95235e8fa84e73a8e9f4524a8a0736
Alex Chika <mcluckey1@gmail.com>
2021-10-06 15:24:04 +0100

    update in progress

--- web.js
+++
@@ -1,5 +0,0 @@
-let navToggle = document.getElementById("nav-toggle");
-let navBtn = document.getElementById("nav-btn");
-navBtn.addEventListener("click", function (e) {
-  navToggle.classList.toggle("nav-toggle");
-});
